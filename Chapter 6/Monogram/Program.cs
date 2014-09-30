ebe0ecacf9e3b8f436d8e19f4146aa97ea9a6a03
buttilloa <anthony.buttillo10@icloud.com>
2014-09-30 08:38:33 -0400

    :a: :id:

---
+++ Chapter 6/Monogram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monogram
+{
+    class Program
+    {
+        public static void PatricksLoveRock(char One, char PatrickStar, char SquidwardsBigOlNose)
+        {
+            Console.WriteLine(" ** " + One + ". " + PatrickStar + ". " + SquidwardsBigOlNose + ". ** ");
+        }
+        static void Main(string[] args)
+        {
+            char CaptRayRayRedBeard = 'A', TheKracken = 'H', TheKrackensMom = 'B', JudgeJudy = 'D', CliffordTheBigRedDog = 'W', BigOlEarLobe = 'H';
+
+            PatricksLoveRock(CaptRayRayRedBeard, TheKracken, TheKrackensMom);
+            PatricksLoveRock(JudgeJudy, CliffordTheBigRedDog, BigOlEarLobe);
+        }
+        
+    }
+    }
+
---
+++ Chapter 6/X-Bone-It/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace X_Bone_It
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Put in (haha put in) a number: ");
+            int MiccioIsSuchAWeenee = Convert.ToInt32(Console.ReadLine());
+        }
+        public static void SpareChairSquareDareFairCareBearSnare(int numbre)
+        {
+        
+        }
+        public static void Pubes(int numbre2)
+        {
+        
+        }
+    }
+}
